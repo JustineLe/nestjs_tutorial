273aee47609acb60fe492c25496c81db8fecf5a3
JustineLe <nghiale1004@gmail.com>
2023-04-04 09:34:10 +0700

    Resquest and Response in nestjs

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-    findCustomer() {
-        return {
+
+    private users = [
+        {
             id: 1,
             email: 'test@gmail.com',
             createdAt: new Date(),
         }
+    ]
+    findCustomerById(id: number) {
+        return this.users.find((user) => user.id === id);
     }
 }
